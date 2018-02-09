5c7d693f06caf4b927d64685885f822f865b8cb0
alvivar <andresalvivar@gmail.com>
2018-02-09 02:30:12 -0600

    A function able to return all files and folders from a path

--- qpost.py
+++ qpost.py
@@ -5,16 +5,37 @@
     compatible json ready to be used.
 """
 
-import eel
+import os
 import time
+
+import eel
 
 eel.init('web')
 
 
 @eel.expose
 def cleantime():
-    return str(round(time.time()))
+    return round(time.time())
 
 
-print(cleantime())
+@eel.expose
+def get_files_dirs(path):
+    """
+        Return a tuple with 2 values, first a list of all files, second a list
+        of all directories.
+    """
+
+    files = []
+    dirs = []
+    for root, ds, fs in os.walk(path, topdown=False):
+        for file_name in fs:
+            files.append(os.path.join(root, file_name))
+        for dir_name in ds:
+            dirs.append(os.path.join(root, dir_name))
+
+    return files, dirs
+
+
 eel.start('app.html', size=(600, 300))
+
+# print(get_files_dirs(r'D:\Dropbox\Public\games\gif'))
