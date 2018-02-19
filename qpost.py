e3ddf8c64bb94bd4f1e9b709a6eb061dee6345cc
alvivar <andresalvivar@gmail.com>
2018-02-18 18:03:00 -0600

    Files are better filtered

--- qpost.py
+++ qpost.py
@@ -5,6 +5,7 @@
     compatible json ready to be used.
 """
 
+import fnmatch
 import os
 import shutil
 import sys
@@ -20,13 +21,13 @@ eel.init('web')
 
 
 @eel.expose
-def get_files_dirs(path, allow=[]):
+def get_files_dirs(path, filesfilter=['*']):
     """
         Return a tuple with 2 values, first a list of all files, second a list
         of all directories.
 
-        'allow' a list of words used to filter the results. Files and
-        directories should contain at least one word.
+        'filesfilter' is a list of file name patterns e.g. '*.gif' used to
+        filter the files results.
     """
 
     files = []
@@ -37,15 +38,41 @@ def get_files_dirs(path, allow=[]):
         for dir_name in ds:
             dirs.append(os.path.join(root, dir_name))
 
-    if allow:
-        files = [f for f in files if any(word in f for word in allow)]
-        dirs = [d for d in dirs if any(word in d for word in allow)]
+    filtered_files = []
+    for pattern in filesfilter:
+        filtered_files.extend(fnmatch.filter(files, pattern))
+    files = filtered_files
 
     return files, dirs
 
 
+def allow_patterns(*patterns):
+    """
+        Function that can be used as copytree() ignore parameter.
+
+        Patterns is a sequence of glob-style patterns that are used to exclude
+        files that don't match.
+    """
+
+    def _ignore_patterns(path, names):
+
+        files_only = [
+            name for name in names
+            if not os.path.isdir(os.path.join(path, name))
+        ]
+
+        allowed_files = []
+        for pattern in patterns:
+            allowed_files.extend(fnmatch.filter(files_only, pattern))
+
+        ignore_others = set(files_only) - set(allowed_files)
+        return ignore_others
+
+    return _ignore_patterns
+
+
 @eel.expose
-def copytree(source):
+def copytree(source, filesfilter=['*']):
     """
         Copy all files from the path into application directory.
     """
@@ -54,7 +81,7 @@ def copytree(source):
     destiny_path = os.path.join(HOME, 'images', destiny)
     if os.path.exists(destiny_path):
         shutil.rmtree(destiny_path)
-    shutil.copytree(source, destiny_path)
+    shutil.copytree(source, destiny_path, ignore=allow_patterns(*filesfilter))
 
 
 # copytree(r'D:\Dropbox\Public\games\gif\haldronmoon')
