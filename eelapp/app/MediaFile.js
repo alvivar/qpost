4e7cedd611ae58be9650c260bb43133f31e1edcd
Andrés Villalobos <andres.villalobos@sweetrush.com>
2019-01-27 13:22:01 -0600

    MP4 support

--- eelapp/app/MediaFile.js
+++ eelapp/app/MediaFile.js
@@ -1,22 +1,22 @@
 class MediaFile extends React.Component {
-  render() {
-    let extension = this.props.filename.split(".").pop();
-    if (extension.toLowerCase() === "webm") {
-      return this.renderWebm();
-    } else {
-      return this.renderImage();
-    }
-  }
+	render() {
+		let extension = this.props.filename.split('.').pop();
+		if ([ 'webm', 'mp4' ].includes(extension.toLowerCase())) {
+			return this.renderWebm();
+		} else {
+			return this.renderImage();
+		}
+	}
 
-  renderWebm() {
-    return (
-      <video controls muted src={this.props.filename}>
-        Sorry, your browser doesn't support embedded videos.
-      </video>
-    );
-  }
+	renderWebm() {
+		return (
+			<video controls muted src={this.props.filename}>
+				Sorry, your browser doesn't support embedded videos.
+			</video>
+		);
+	}
 
-  renderImage() {
-    return <img src={this.props.filename} />;
-  }
+	renderImage() {
+		return <img src={this.props.filename} />;
+	}
 }
